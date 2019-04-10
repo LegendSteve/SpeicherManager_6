0c06210c4c2d185da96ec0d7e6013898927496af
LegendSteve <46540640+LegendSteve@users.noreply.github.com>
2019-04-10 19:36:55 +0200

    Add files via upload

---
+++ 10minutemail.js
@@ -0,0 +1,16 @@
+/*script = document.createElement("script");
+script.innerHTML = "function receiveMessage(event){if(event.data != 'Zahl bekommen')return;var zahl = $('tr').find('td:contains('Verwenden Sie zum Bestätigen Ihrer E-Mail-Adresse den folgenden Sicherheitscode: ')').children()[0].innerHTML; event.source.postMessage('Zahl: '+zahl, event.origin); window.close();} window.addEventListener('message', receiveMessage, false);";
+document.body.appendChild(script);*/
+
+function receiveMessage(event){
+    if(event.data != 'Zahl bekommen')
+    return;
+    
+    var zahl = $('tr').find("td:contains('Verwenden Sie zum Bestätigen Ihrer E-Mail-Adresse den folgenden Sicherheitscode: ')").children()[0].innerHTML;
+    event.source.postMessage('Zahl: '+zahl, event.origin);
+    window.close();
+}
+
+window.addEventListener('message', receiveMessage, false);
+
+//https://raw.githack.com/LegendSteve/SpeicherManager_1/master/10minutemail.js
